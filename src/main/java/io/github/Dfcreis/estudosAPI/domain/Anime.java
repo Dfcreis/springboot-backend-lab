3f07709f3a8baab3a60f5870b19b62c58c15bc5a
Daniel <danieldfcreis@gmail.com>
2026-03-31 22:44:11 -0300

    2# Aprendendo anotacoes basicas

--- src/main/java/io/github/Dfcreis/estudosAPI/domain/Anime.java
+++ src/main/java/io/github/Dfcreis/estudosAPI/domain/Anime.java
@@ -9,4 +9,11 @@ public class Anime {
     }
     public Anime(){}
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/io/github/Dfcreis/estudosAPI/start/ApplicationStart.java
+++ src/main/java/io/github/Dfcreis/estudosAPI/start/ApplicationStart.java
@@ -1,13 +1,21 @@
 package io.github.Dfcreis.estudosAPI.start;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
+
+@SpringBootApplication
+//Anotação para indicar que esta classe é a classe principal de configuração do Spring Boot,
+// ou seja, ela é responsável por iniciar a aplicação e configurar os componentes do Spring.
+
+@ComponentScan(basePackages = "io.github.Dfcreis.estudosAPI")
+// Especifica os pacotes onde o Spring deve procurar por componentes, como controladores, serviços, repositórios, etc.
+// Neste caso, ele irá escanear o pacote "io.github.Dfcreis.estudosAPI" e seus subpacotes para encontrar os componentes do Spring.
 public class ApplicationStart {
+
+
     public static void main(String[] args) {
-
         SpringApplication.run(ApplicationStart.class, args);
-
     }
 }
