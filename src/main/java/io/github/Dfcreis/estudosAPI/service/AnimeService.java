9c02265fc3cdf61a586b0c887742f18aa8e3b59d
Daniel <danieldfcreis@gmail.com>
2026-04-03 08:08:50 -0300

    Criando metodo post

--- src/main/java/io/github/Dfcreis/estudosAPI/service/AnimeService.java
+++ src/main/java/io/github/Dfcreis/estudosAPI/service/AnimeService.java
@@ -5,12 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service// Anotação para indicar que esta classe é um serviço, ou seja,
 // ela contém a lógica de negócios da aplicação.
 public class AnimeService{
-    private List<Anime> animes = List.of(new Anime(1L,"Berserk"), new Anime(2L,"One Piece"));
+    private static List<Anime> animes ;
+
+    static {
+        animes = new ArrayList<> (List.of(new Anime(1L,"Berserk"), new Anime(2L,"One Piece")));
+    }
 
     public List<Anime> listAll(){
         return animes;
@@ -22,4 +28,11 @@ public class AnimeService{
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
+
+    public Anime save(Anime anime ){
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000));
+        animes.add(anime);
+        return anime;
+    }
+
 }
